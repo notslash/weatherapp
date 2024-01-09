e10d34df3915a381dd3c329646411534207328df
Dillan Patel (40186346) <40186346@hruc.ac.uk>
2024-01-09 11:02:23 +0000

    changes

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import "./Header.css"
+
+const Header=()=>{
+    return(
+        <div className="background">
+           
+            <p className="header_text">
+                Health advice group   
+            </p>
+            
+         
+            <div className="underline">
+        </div>  
+        </div>
+    )
+}
+
+export default Header;
