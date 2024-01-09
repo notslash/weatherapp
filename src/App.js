b72d17d6d735ca31dd59916bae5bb5ba1fd1ed6e
Dillan Patel (40186346) <40186346@hruc.ac.uk>
2024-01-09 16:27:34 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -16,10 +16,10 @@ function App() {
       const lat=position.coords.latitude 
       const long=position.coords.longitude
     
-      const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${[process.env.REACT_APP_OPENWEATHERMAP_KEY]}&units=metric`)
+      const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_OPENWEATHERMAP_KEY}&units=metric`)
 
 
-    console.log(res.data)
+      console.log(res.data)
 
       setWeather_data(res.data)
     } catch (err) {
@@ -31,8 +31,17 @@ function App() {
   return (
     <div className="background">
       <Header/>
-      {weather_data && <div className="temp">{weather_data.temp}</div>}
+      {weather_data && <div>
+        <div className="tem">
+          <img id="test"src={`https://openweathermap.org/img/wn/${weather_data.weather[0].icon}@2x.png`} /> 
+          <div className="location">{weather_data.name}, {weather_data.weather[0].description}</div>
+        </div>
 
+  
+        <div className="temp">{Math.round(weather_data.main.temp)}°</div>
+        </div>}
+
+      
 
     
     </div>
