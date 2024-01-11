2c7e7c4e6dbdb9ecee7ea32ea8b38df8b8d6724c
notslash <dillanpatel845@gmail.com>
2024-01-11 11:55:32 +0000

    changes `

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [location_data, setLocation_data]=useState(null)
   const [weather_data, setWeather_data]=useState(null)
   const [isLoading, setIsLoading] = useState(false)
+  const [forc, setForc]=useState(null)
 
   useEffect(()=>{navigator.geolocation.getCurrentPosition(async(position)=>{const lat=position.coords.latitude 
     const long=position.coords.longitude
@@ -17,32 +18,53 @@ function App() {
       const long=position.coords.longitude
     
       const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_OPENWEATHERMAP_KEY}&units=metric`)
+      const forcast = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_OPENWEATHERMAP_KEY}&units=metric`)
 
 
       console.log(res.data)
-
+      console.log(forcast.data)
       setWeather_data(res.data)
+      setForc(forcast.data)
     } catch (err) {
       console.error(err)
     } finally {
       setIsLoading(false)
     }})}, [])
     
+    const format_day=(timestamp, formatStyle)=>{
+    const day =new Date(timestamp * 1000).toLocaleDateString([], {weekday: formatStyle})
+      return day
+    }
+
   return (
     <div className="background">
       <Header/>
       {weather_data && <div>
         <div className="tem">
           <img id="test"src={`https://openweathermap.org/img/wn/${weather_data.weather[0].icon}@2x.png`} /> 
-          <div className="location">{weather_data.name}, {weather_data.weather[0].description}</div>
+          <div className="location">{weather_data.name}, {format_day(weather_data.dt, "long")}</div>
         </div>
 
   
         <div className="temp">{Math.round(weather_data.main.temp)}°</div>
-        </div>}
+
+        <div className="columns">
+
+        <div className="today">{format_day(weather_data.dt, "short")}</div>
+
+        </div>
+
+        <div>{forc && <div>
+          <div className="forcast">{forc.list.map((weather, i)=>{if (i%8==0){return <div key={i}>{new Date(weather.dt * 1000).toLocaleTimeString([], {hour: "2-digit", minute: "2-digit"})} PM</div>}else
+      {return null}})}
+          {new Date(weather.dt * 1000).list.main.temp}
+      </div>
+          </div> }</div>
+        </div>
+        }
 
       
-
+        
     
     </div>
   );
