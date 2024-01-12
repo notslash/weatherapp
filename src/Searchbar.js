4b4a4810d5cb75538d8703b1e4bba20ff6e6912e
Dillan Patel (40186346) <40186346@hruc.ac.uk>
2024-01-12 15:23:38 +0000

    search bar done almost

--- src/Searchbar.js
+++ src/Searchbar.js
@@ -1,17 +1,34 @@
 import "./Searchbar.css"
 
 import { useState } from "react";
+import axios from "axios";
 
 
-const Searchbar =()=>{ 
-    const [CityName,SetCityName]=useState('london')
+const Searchbar = () => {
+    const [CityName, SetCityName] = useState("")
+    const [locationData, setLocationData] = useState(null)
+    const [lat, SetLat]=useState()
+    const [long,SetLong]=useState()
 
-    return(
-        <div>
-            <form className="bar">
-                <input onSubmit={`https://api.openweathermap.org/data/2.5/weather?city=${CityName}&appid=${process.env.REACT_APP_OPENWEATHERMAP_KEY}&units=metric`} id="search_bar" type="text" placeholder="Search"/>
-                    
+    return (
+        <div className="searchbarcont">
+            <form className="bar" onSubmit={async (e) => { 
+                e.preventDefault();
+                console.log(CityName)
+                const res = await axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${CityName}&limit=5&appid=${process.env.REACT_APP_OPENWEATHERMAP_KEY}`)
+                setLocationData([...res.data])
+                
+                // const loc = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_OPENWEATHERMAP_KEY}&units=metric`)
+                // console.log(loc.data)
+                } 
+                
+            }>
+                <input onChange={(e)=>SetCityName(e.target.value)} value={CityName} id="search_bar" type="text" placeholder="Search" />
+
             </form>
+            <div >{locationData && locationData.map((location, i) => {
+               return location.name
+            })}</div>
         </div>
     )
 }
