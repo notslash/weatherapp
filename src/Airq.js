cd9abba4d79ffa515200fdcf2460c372e31c34e5
notslash <dillanpatel845@gmai.com>
2024-01-23 16:01:25 +0000

    changes

--- src/Airq.js
+++ src/Airq.js
@@ -28,8 +28,20 @@ const Airq=()=>{
     <div className="background_air">
     
     {display&& <div className="airquality">
-        Carbon Monoxide {display.list[0].components.co}
-        ammonia {display.list[0].components.nh3}
+        <div className="carbon" role="progressbar">Carbon Monoxide {display.list[0].components.co}
+        <progress value={display.list[0].components.so2} max="4">{display.list[0].components.co}</progress></div>
+        <div className="ammonia">Ammonia {display.list[0].components.nh3}
+        <progress value={display.list[0].components.nh3} max="4">{display.list[0].components.nh3}</progress></div>
+        <div className="nitrogen">nitrogen dioxide {display.list[0].components.n02}
+        <progress value={display.list[0].components.o3} max="100">{display.list[0].components.n02}</progress></div>
+        <div className="ozon">Ozone {display.list[0].components.o3}
+        <progress value={display.list[0].components.o3} max="4">{display.list[0].components.o3}</progress></div>
+        <div className="particals_2.5">particals(2.5µm) {display.list[0].components.pm2_5}
+        <progress value={display.list[0].components.pm2_5} max="4">{display.list[0].components.pm2_5}</progress></div>
+        <div className="particlas_10">particals(10µm) {display.list[0].components.pm10}
+        <progress value={display.list[0].components.pm10} max="4">{display.list[0].components.pm10}</progress></div>
+        <div className="sulphur">Sulphur {display.list[0].components.so2}
+        <progress value={display.list[0].components.so2} max="4">{display.list[0].components.so2}</progress></div>
 
         </div>}
     </div>
