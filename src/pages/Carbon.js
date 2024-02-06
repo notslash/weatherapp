2459c55e6320134d22bd7fd17171b0d3d5a933d0
Dillan Patel (40186346) <40186346@hruc.ac.uk>
2024-02-06 12:55:33 +0000

    changes

--- src/pages/Carbon.js
+++ src/pages/Carbon.js
@@ -4,12 +4,20 @@ const Carbon=()=>{
         <div className="background">
             <h2 className="carbon_header">Carbon Monoxide</h2>
             <div className="grid_cotainer">
-                <div className="grid_item">0-35 ppm</div>
-                <div className="grid_item">0-35 ppm</div>
-                <div className="grid_item">0-35 ppm</div>
-                <div className="grid_item">0-35 ppm</div>
-                <div className="grid_item">0-35 ppm</div>
-                <div className="grid_item">0-35 ppm</div>
+                <div className="grid_item">0-6 ppm</div>
+                <div className="grid_item">10-35 ppm</div>
+                <div className="grid_item">35-70 ppm</div>
+                <div className="grid_item">100-200 ppm</div>
+                <div className="grid_item">200-400 ppm</div>
+                <div className="grid_item">800+ ppm</div>
+                <div className="grid_item">Normal carbon levels</div>
+                <div className="grid_item">You may expience dizzyness or feel nousious</div>
+                <div className="grid_item">We recommend you do not say in these conditions for more then 8 hours</div>
+                <div className="grid_item">You may expience physical symptops after 6-8 hours</div>
+                <div className="grid_item">You may expience physical symptops after 3-5 hours</div>
+                <div className="grid_item">You may expience physical symptops after 2-3 hours</div>
+                <div>You can stay in these conditions for more then 9 hours</div>
+                <div className="grid_item">You may expience physical symptops after 20 minutes</div>
 
             </div>
         
